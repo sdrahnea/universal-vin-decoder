a750e63b7c7a7704014e6811dbeb81819eb13417
Sergiu Drahnea <s.drahnea@gmail.com>
2019-05-22 08:16:08 +0300

    add hyundai decoder

--- src/main/java/com/uvd/controller/ConstantController.java
+++ src/main/java/com/uvd/controller/ConstantController.java
@@ -1,6 +1,7 @@
 package com.uvd.controller;
 
 import com.uvd.model.Constant;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,8 +10,5 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping(value = "/constant")
-
-
-@Ser
 public class ConstantController extends AbstractController<Constant> {
 }
